667abb28f421a941dfe8cd56e215497cd0a05e96
Hannah Bollar <hanbollar@gmail.com>
2018-11-07 18:06:31 -0500

    add - npm project setup from a differ proj

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import { makeRenderLoop, camera, cameraControls, gui, gl } from './init';
+import ForwardRenderer from './renderers/forward';
+import ForwardPlusRenderer from './renderers/forwardPlus';
+import ClusteredRenderer from './renderers/clustered';
+import Scene from './scene';
+
+const FORWARD = 'Forward';
+const FORWARD_PLUS = 'Forward+';
+const CLUSTERED = 'Clustered';
+
+const params = {
+  renderer: FORWARD_PLUS,
+  _renderer: null,
+};
+
+setRenderer(params.renderer);
+
+function setRenderer(renderer) {
+  switch(renderer) {
+    case FORWARD:
+      params._renderer = new ForwardRenderer();
+      break;
+    case FORWARD_PLUS:
+      params._renderer = new ForwardPlusRenderer(15, 15, 15);
+      break;
+    case CLUSTERED:
+      params._renderer = new ClusteredRenderer(15, 15, 15);
+      break;
+  }
+}
+
+gui.add(params, 'renderer', [FORWARD, FORWARD_PLUS, CLUSTERED]).onChange(setRenderer);
+
+const scene = new Scene();
+scene.loadGLTF('models/sponza/sponza.gltf');
+
+camera.position.set(-10, 8, 0);
+cameraControls.target.set(0, 2, 0);
+gl.enable(gl.DEPTH_TEST);
+
+function render() {
+  scene.update();
+  params._renderer.render(camera, scene);
+}
+
+makeRenderLoop(render)();
---
+++ src/renderers/forwardPlus.js
@@ -0,0 +1,93 @@
+import { gl } from '../init';
+import { mat4, vec4, vec3, vec2 } from 'gl-matrix';
+import { loadShaderProgram } from '../utils';
+import { NUM_LIGHTS } from '../scene';
+import { MAX_LIGHTS_PER_CLUSTER } from './base.js'
+import vsSource from '../shaders/forwardPlus.vert.glsl';
+import fsSource from '../shaders/forwardPlus.frag.glsl.js';
+import TextureBuffer from './textureBuffer';
+import BaseRenderer from './base';
+
+export default class ForwardPlusRenderer extends BaseRenderer {
+  constructor(xSlices, ySlices, zSlices) {
+    super(xSlices, ySlices, zSlices);
+
+    // Create a texture to store light data
+    this._lightTexture = new TextureBuffer(NUM_LIGHTS, 8);
+    
+    this._shaderProgram = loadShaderProgram(vsSource, fsSource({
+      numLights: NUM_LIGHTS,
+      numLights_perCluster: MAX_LIGHTS_PER_CLUSTER,
+      slices_x: xSlices,
+      slices_y: ySlices,
+      slices_z: zSlices
+    }), {
+      uniforms: ['u_viewProjectionMatrix', 'u_colmap', 'u_normap', 'u_lightbuffer', 'u_clusterbuffer',
+                 'u_view_matrix', 'u_screen_dimensions', 'u_near_clip', 'u_far_clip'],
+      attribs: ['a_position', 'a_normal', 'a_uv'],
+    });
+
+    this._projectionMatrix = mat4.create();
+    this._viewMatrix = mat4.create();
+    this._viewProjectionMatrix = mat4.create();
+  }
+
+  render(camera, scene) {
+    // Update the camera matrices
+    camera.updateMatrixWorld();
+    mat4.invert(this._viewMatrix, camera.matrixWorld.elements);
+    mat4.copy(this._projectionMatrix, camera.projectionMatrix.elements);
+    mat4.multiply(this._viewProjectionMatrix, this._projectionMatrix, this._viewMatrix);
+
+    // Update cluster texture which maps from cluster index to light list
+    this.updateClusters(camera, this._viewMatrix, scene);
+    
+    // Update the buffer used to populate the texture packed with light data
+    for (let i = 0; i < NUM_LIGHTS; ++i) {
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 0) + 0] = scene.lights[i].position[0];
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 0) + 1] = scene.lights[i].position[1];
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 0) + 2] = scene.lights[i].position[2];
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 0) + 3] = scene.lights[i].radius;
+
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 1) + 0] = scene.lights[i].color[0];
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 1) + 1] = scene.lights[i].color[1];
+      this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 1) + 2] = scene.lights[i].color[2];
+    }
+    // Update the light texture
+    this._lightTexture.update();
+
+    // Bind the default null framebuffer which is the screen
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    // Render to the whole screen
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Clear the frame
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Use this shader program
+    gl.useProgram(this._shaderProgram.glShaderProgram);
+
+    // Upload the camera matrix
+    gl.uniformMatrix4fv(this._shaderProgram.u_viewProjectionMatrix, false, this._viewProjectionMatrix);
+    gl.uniformMatrix4fv(this._shaderProgram.u_view_matrix, false, this._viewMatrix);
+
+    // Set the light texture as a uniform input to the shader
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, this._lightTexture.glTexture);
+    gl.uniform1i(this._shaderProgram.u_lightbuffer, 2);
+
+    // Set the cluster texture as a uniform input to the shader
+    gl.activeTexture(gl.TEXTURE3);
+    gl.bindTexture(gl.TEXTURE_2D, this._clusterTexture.glTexture);
+    gl.uniform1i(this._shaderProgram.u_clusterbuffer, 3);
+
+    // Bind any other shader inputs
+    gl.uniform2f(this._shaderProgram.u_screen_dimensions, canvas.width, canvas.height);
+    gl.uniform1f(this._shaderProgram.u_near_clip, camera.near); 
+    gl.uniform1f(this._shaderProgram.u_far_clip, camera.far);
+
+    // Draw the scene. This function takes the shader program so that the model's textures can be bound to the right inputs
+    scene.draw(this._shaderProgram);
+  }
+};
